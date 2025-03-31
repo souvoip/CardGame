6dfdcba1f0785ca3877d99c50f6760abcd1d6219
souvoip <2962198972@qq.com>
2025-04-01 00:31:41 +0800

    优化代码

--- Assets/Scripts/Game/Card/CardManager.cs
+++ Assets/Scripts/Game/Card/CardManager.cs
@@ -301,7 +301,7 @@ public class CardManager : MonoBehaviour
         {
             return;
         }
-
+        if (temporaryCard.activeSelf) { return; }
         oldmousePosition = Input.mousePosition;
         // 从鼠标位置创建一条射线
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
